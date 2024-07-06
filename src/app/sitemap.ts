2690697d1a4ba77cb9f46599087be201422a1b67
OHS <ohs6006@gmail.com>
2024-07-06 21:37:08 +0900

    Chore : sitemap 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,32 @@
+import { getPostList } from "@/lib/blog";
+import { MetadataRoute } from "next";
+
+const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const defaultSitemap = [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+  ];
+
+  const allPost = await getPostList();
+
+  const allPostSitemaps = allPost.map((post) => {
+    return {
+      url: `${BASE_URL}/${post.category}/${post.title}`,
+      lastModified: new Date(),
+    };
+  });
+
+  return [...defaultSitemap, ...allPostSitemaps];
+}
