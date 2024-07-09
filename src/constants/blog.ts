b08a7a1f626d8f7e673c76feb5314178d3be09bf
OHS <ohs6006@gmail.com>
2024-07-09 19:09:46 +0900

    Chore : og이미지 수정

--- src/constants/blog.ts
+++ src/constants/blog.ts
@@ -8,5 +8,6 @@ export const META = {
   description:
     "프론트엔드 라는 퍼즐을 한 조각 한 조각씩 맞춰가고 있는 중입니다. 이 블로그는 프론트엔드 개발 여정을 기록하고, 배운 것들을 공유하기 위해 만들었습니다.",
   url: "https://www.puzzler.life/",
-  ogImage: "",
+  ogImage:
+    "https://res.cloudinary.com/dxesudkxn/image/upload/v1720519640/blog/dwkzv7niwgk9j3wx5syh.png",
 } as const;
