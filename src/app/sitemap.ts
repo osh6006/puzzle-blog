1af981a9719ffbddd68377f0d141ad62f87fe4e5
OHS <ohs6006@gmail.com>
2024-08-05 04:43:35 +0900

    Modify : sitemap 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,27 +5,29 @@ const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : "https://www.puzzler.life";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const defaultSitemap = [
+  const defaultSitemap: MetadataRoute.Sitemap = [
     {
       url: BASE_URL,
       lastModified: new Date(),
-      changeFrequency: "daily",
+      changeFrequency: "daily", // 문자열 리터럴 값
       priority: 1,
     },
     {
       url: `${BASE_URL}/blog`,
       lastModified: new Date(),
-      changeFrequency: "daily",
+      changeFrequency: "daily", // 문자열 리터럴 값
     },
   ];
 
   const allPost = await getPostList();
 
-  const allPostSitemaps = allPost.map((post) => {
+  const allPostSitemaps: MetadataRoute.Sitemap = allPost.map((post) => {
     return {
       url: `${BASE_URL}/${post.url}`,
       lastModified: new Date(),
+      changeFrequency: "daily", // 문자열 리터럴 값
     };
   });
 
